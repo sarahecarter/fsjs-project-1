d0fd50a83f4fe4bb006e0f7eba9e5357c8fffc4a
sarahecarter <scarter3@conncoll.edu>
2021-01-26 18:50:20 -0500

    added JS functions

--- js/script.js
+++ js/script.js
@@ -43,14 +43,67 @@ const quotes = [
 /***
  * `getRandomQuote` function
 ***/
-
+function getRandomQuote() {
+  //Get a random number from 0 to the last item in the array
+  let randomNumber = Math.floor(Math.random() * quotes.length);
+  //Use random number to return a random quote from array
+  let randomQuote = quotes[randomNumber];
+  //Return the random quote
+  return randomQuote;
+}
 
 
 /***
  * `printQuote` function
 ***/
+function printQuote() {
+  //Call the getRandomQuote function and store quote in variable
+  let quoteObject = getRandomQuote();
+  //Use the returned quote to build a string of HTML
+  let htmlString = `<p class="quote">${quoteObject.quote}</p>
+                    <p class="source">${quoteObject.source}`;
+  //If quote has citation create a citation span
+  if(quoteObject.citation !== undefined) {
+    htmlString += `<span class="citation">${quoteObject.citation}</span>`
+  } 
+  //If the quote has month create a month span
+  if(quoteObject.month !== undefined) {
+    htmlString += `<span class="year">${quoteObject.month}</span>`;
+  } 
+  //If the quote has year create a year span
+  if(quoteObject.year !== undefined) {
+    htmlString += `<span class="year">${quoteObject.year}</span>`;
+  } 
+  //Close <p> tag
+  htmlString += `</p>`;
+  //Use that string to display a random quote in the browser
+  document.getElementById('quote-box').innerHTML = htmlString; 
+  //Call random color function
+  getRandomColor();
+  //Return HTML
+  return htmlString;
+  
+}
+
+/***
+ * `getRandomColor` function
+***/
+//Idea from https://www.w3resource.com/javascript-exercises/javascript-math-exercise-40.php
+
+function getRandomColor() {
+  //Get 3 random numbers between 0 and 255
+  let r = Math.floor(Math.random() * 255);
+  let g = Math.floor(Math.random() * 255);
+  let b = Math.floor(Math.random() * 255);
+  //Create RGB color
+  let color = `rgb(${r},${g},${b})`;
+  //Set the background of the page to the color
+  document.body.style.background = color;
+}
 
 
+//Set interval so printQuote is called every 10 seconds
+setInterval(printQuote, 10000);
 
 /***
  * click event listener for the print quote button
